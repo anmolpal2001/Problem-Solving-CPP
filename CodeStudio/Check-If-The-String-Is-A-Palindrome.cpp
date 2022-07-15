732bf24b089eb7aeb17e0c4d30d546db0c3cdaef
Anmol Pal <85279423+anmolpal2001@users.noreply.github.com>
2022-07-15 13:07:16 +0530

    Update Check-If-The-String-Is-A-Palindrome.cpp

--- CodeStudio/Check-If-The-String-Is-A-Palindrome.cpp
+++ CodeStudio/Check-If-The-String-Is-A-Palindrome.cpp
@@ -34,6 +34,37 @@ bool isPalindrome(string s)
     return 1;
 }
 
+// Solution 2 : 
+bool checkPalindrome(string s)
+{
+    string temp;
+    for(int i=0;i<s.length();i++)
+    {
+        if((s[i]>='a' && s[i]<='z') || (s[i]>='A' && s[i]<='Z') || (s[i]>='0' && s[i]<='9'))
+        {
+            temp.push_back(s[i]);
+        }
+    }
+    for(int i=0;i<temp.length();i++)
+    {
+        temp[i]=tolower(temp[i]);
+    }
+    int st=0;
+    int e=temp.length()-1;
+    while(st<=e)
+    {
+        if(temp[st]!=temp[e])
+        {
+            return 0;
+        }
+        else
+        {
+            st++;
+            e--;
+        }
+    }
+    return 1;
+}
 
 int main()
 {
