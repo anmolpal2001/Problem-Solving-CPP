980250ccbfc8a301fec49a9c448b0b99373d5e6a
Anmol Pal <85279423+anmolpal2001@users.noreply.github.com>
2022-07-15 18:28:48 +0530

    Create Maximum-Occuring-Character .cpp

---
+++ GeeksforGeeks/Maximum-Occuring-Character .cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    char getMaxOccuringChar(string str)
+    {
+        int arr[26] = {0};
+        int number = 0;
+        for (int i = 0; i < str.length(); i++)
+        {
+            if (str[i] >= 'a' && str[i] <= 'z')
+            {
+                number = str[i] - 'a';
+            }
+            arr[number]++;
+        }
+        int maxi = -1, ans = 0;
+        for (int i = 0; i < 26; i++)
+        {
+            if (maxi < arr[i])
+            {
+                ans = i;
+                maxi = arr[i];
+            }
+        }
+        char finalAns = 'a' + ans;
+        return finalAns;
+    }
+};
+int main()
+{
+    string str;
+    cin >> str;
+    Solution obj;
+    cout << obj.getMaxOccuringChar(str) << endl;
+}
