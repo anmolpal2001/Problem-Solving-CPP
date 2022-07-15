54554d68fe79fa50ab1577e72ce98f816365d0f5
Anmol Pal <85279423+anmolpal2001@users.noreply.github.com>
2022-07-15 23:02:12 +0530

    Create 75. Sort_Colors.cpp

---
+++ Leetcode/Array/75. Sort_Colors.cpp
@@ -0,0 +1,22 @@
+Question : https://leetcode.com/problems/sort-colors/
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int lo=0;
+        int mid=0;
+        int hi=nums.size()-1;
+        while(mid<=hi)
+        {
+            switch(nums[mid])
+            {
+                case 0: swap(nums[mid++],nums[lo++]);
+                break;
+                case 1: mid++;
+                break;
+                case 2: swap(nums[mid],nums[hi--]);
+                break;
+            }
+        }
+    }
+};
