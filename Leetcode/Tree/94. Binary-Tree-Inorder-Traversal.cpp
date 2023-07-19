ce7d3b39421fad1c384ae596677d9d7b1ea82087
Anmol Pal <85279423+anmolpal2001@users.noreply.github.com>
2023-07-19 16:23:32 +0530

    Create 94. Binary-Tree-Inorder-Traversal.cpp

---
+++ Leetcode/Tree/94. Binary-Tree-Inorder-Traversal.cpp
@@ -0,0 +1,19 @@
+Ques : https://leetcode.com/problems/binary-tree-inorder-traversal/
+
+Solution : 
+class Solution {
+    private:
+    void inorder(TreeNode* &root,vector<int> &ans){
+    
+        if(!root) return;
+        inorder(root->left,ans);
+        ans.push_back(root->val);
+        inorder(root->right,ans);
+    }
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        inorder(root,ans);
+        return ans;
+    }
+};
