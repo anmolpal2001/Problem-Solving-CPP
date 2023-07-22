9798bac60953dabccb970c9dd66dc70547ef59fd
Anmol Pal <85279423+anmolpal2001@users.noreply.github.com>
2023-07-22 22:28:46 +0530

    Create 103. Binary-Tree-Zigzag-Level-Order-Traversal.cpp

---
+++ Leetcode/Tree/103. Binary-Tree-Zigzag-Level-Order-Traversal.cpp
@@ -0,0 +1,33 @@
+Ques : https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/
+
+Solution : 
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+        if(root == NULL)
+            return ans;
+        queue <TreeNode*> q;
+        q.push(root);
+        bool flag=true;
+        while(!q.empty())
+        {
+            int size = q.size();
+            vector<int> row(size);
+            for(int i=0;i<size;i++)
+            {
+                TreeNode* curr = q.front();
+                q.pop();
+                int index = flag ? i : (size-1-i);
+                row[index]=curr->val;
+                if(curr->left)
+                    q.push(curr->left);
+                if(curr->right)
+                    q.push(curr->right);
+            }
+            flag = !flag;
+            ans.push_back(row);
+        }
+        return ans;
+    }
+};
