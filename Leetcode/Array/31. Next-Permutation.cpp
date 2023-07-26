9afb4347952d02eb38a7136ce98bf3ca9456e4ce
Anmol Pal <85279423+anmolpal2001@users.noreply.github.com>
2023-07-27 03:27:41 +0530

    Update 31. Next-Permutation.cpp

--- Leetcode/Array/31. Next-Permutation.cpp
+++ Leetcode/Array/31. Next-Permutation.cpp
@@ -16,7 +16,7 @@ public:
             reverse(nums.begin(), nums.end());
         }
         else {
-            for (int i = n - 1; i >= 0; i--) {
+            for (int i = n - 1; i >= ind; i--) {
                 if (nums[i] > nums[ind]) {
                     swap(nums[i], nums[ind]);
                     break;
