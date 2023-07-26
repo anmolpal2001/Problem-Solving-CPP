ca2643a0189839f2f673ff8e797be9c8e46c0578
Anmol Pal <85279423+anmolpal2001@users.noreply.github.com>
2023-07-26 13:26:49 +0530

    Update 144. Binary-Tree-Preorder-Traversal.cpp

--- Leetcode/Tree/144. Binary-Tree-Preorder-Traversal.cpp
+++ Leetcode/Tree/144. Binary-Tree-Preorder-Traversal.cpp
@@ -1,6 +1,7 @@
 Ques : https://leetcode.com/problems/binary-tree-preorder-traversal/
 
 Solution : 
+// Using Recursion
 class Solution {
     private:
     void preorder(TreeNode* root,vector<int>&ans)
@@ -15,6 +16,28 @@ public:
         vector<int> ans;
         preorder(root,ans);
 
+        return ans;
+    }
+};
+
+// Using Iteration
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        if(root==NULL) return ans;
+        stack<TreeNode*> st;
+        st.push(root);
+        while(st.empty()==false)
+        {
+            root = st.top();
+            st.pop();
+            ans.push_back(root->val);
+            if(root->right !=NULL)
+                st.push(root->right);
+            if(root->left != NULL)
+                st.push(root->left);
+        }
         return ans;
     }
 };
