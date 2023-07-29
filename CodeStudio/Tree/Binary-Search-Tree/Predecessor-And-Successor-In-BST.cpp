b84a5e80e5b5a5a98292d63b2b6e09c9f97ccfe0
Anmol Pal <85279423+anmolpal2001@users.noreply.github.com>
2023-07-29 18:17:00 +0530

    Create Predecessor-And-Successor-In-BST.cpp

---
+++ CodeStudio/Tree/Binary-Search-Tree/Predecessor-And-Successor-In-BST.cpp
@@ -0,0 +1,37 @@
+Ques : https://www.codingninjas.com/studio/problems/predecessor-and-successor-in-bst_893049
+
+Solution : 
+pair<int,int> predecessorSuccessor(BinaryTreeNode<int>* root, int key)
+{
+    int pre = -1;
+    int succ = -1;
+    BinaryTreeNode<int>* temp = root;
+    while(temp->data!=key)
+    {
+        if(temp->data > key)
+        {
+            succ = temp->data;
+            temp=temp->left;
+        }
+        else
+        {
+            pre = temp->data;
+            temp=temp->right;
+        }
+    }   
+    BinaryTreeNode<int>* leftTree = temp->left;
+    while(leftTree!=NULL)
+    {
+        pre=leftTree->data;
+        leftTree = leftTree->right;
+    }
+    BinaryTreeNode<int>* rightTree = temp->right;
+    while(rightTree!=NULL)
+    {
+        succ=rightTree->data;
+        rightTree=rightTree->left;
+    }
+
+    // pair<int,int> ans = make_pair(pre,succ);
+    return {pre,succ};
+}
