e150c2ecc56183c0cb3ca23d5216ed08379e94ea
Anmol Pal <85279423+anmolpal2001@users.noreply.github.com>
2023-07-29 17:10:06 +0530

    Update 98. Validate-Binary-Search-Tree.cpp

--- Leetcode/Tree/Binary-Search-Tree/98. Validate-Binary-Search-Tree.cpp
+++ Leetcode/Tree/Binary-Search-Tree/98. Validate-Binary-Search-Tree.cpp
@@ -7,18 +7,12 @@ public:
     {
         if(root==NULL) return true;
         if(root->val > min && root->val < max){
-            bool left = checkBST(root->left,min,root->val);
-            bool right = checkBST(root->right,root->val,max);
-            return left&&right;
+            return checkBST(root->left,min,root->val) && checkBST(root->right,root->val,max);
         }
         return false;
 
     }
     bool isValidBST(TreeNode* root) {
-        if(root==NULL)
-            return true;
-        if(root->left==NULL && root->right ==NULL)
-            return true;
         return checkBST(root,LONG_MIN,LONG_MAX);
     }
 };
