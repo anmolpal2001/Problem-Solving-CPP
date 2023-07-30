4d684f17ba501d61b5cbceed5943b34133c71707
Anmol Pal <85279423+anmolpal2001@users.noreply.github.com>
2023-07-30 23:37:11 +0530

    Create Flatten-BST-To-A-Sorted-List.cpp

---
+++ CodeStudio/Tree/Binary-Search-Tree/Flatten-BST-To-A-Sorted-List.cpp
@@ -0,0 +1,29 @@
+Ques : https://www.codingninjas.com/studio/problems/flatten-bst-to-a-sorted-list_1169459
+
+Solution : 
+void inorder(TreeNode<int>* root,vector<int>&ans)
+{
+    if(root == NULL) 
+        return;
+    inorder(root->left,ans);
+    ans.push_back(root->data);
+    inorder(root->right,ans);
+}
+TreeNode<int>* flatten(TreeNode<int>* root)
+{
+    vector<int> ans;
+    inorder(root,ans);
+    int n=ans.size();
+    TreeNode<int>* newRoot = new TreeNode<int>(ans[0]);
+    TreeNode<int>* curr = newRoot;
+    for(int i=1;i<n;i++)
+    {
+        TreeNode<int>* temp = new TreeNode<int>(ans[i]);
+        curr->left=NULL;
+        curr->right=temp;
+        curr=temp;
+    }
+    curr->left=NULL;
+    curr->right=NULL;
+    return newRoot;
+}
