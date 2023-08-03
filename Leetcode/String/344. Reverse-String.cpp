400c8eba42e513f6860993dba91557d3eee30185
Anmol Pal <85279423+anmolpal2001@users.noreply.github.com>
2023-08-04 00:10:02 +0530

    Create 344. Reverse-String.cpp

---
+++ Leetcode/String/344. Reverse-String.cpp
@@ -0,0 +1,14 @@
+Ques : https://leetcode.com/problems/reverse-string/
+
+Solution : 
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int start=0;
+        int end=s.size()-1;
+        while(start<end)
+        {
+            swap(s[start++],s[end--]);
+        }
+    }
+};
