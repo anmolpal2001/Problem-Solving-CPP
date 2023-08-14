1fb890be5452fedf6fd62200fc64f8be078437f7
Anmol Pal <85279423+anmolpal2001@users.noreply.github.com>
2023-08-14 23:03:52 +0530

    Create 1920. Build-Array-from-Permutation.cpp

---
+++ Leetcode/Array/1920. Build-Array-from-Permutation.cpp
@@ -0,0 +1,15 @@
+Ques : https://leetcode.com/problems/build-array-from-permutation/
+
+Solution : 
+// TC : O(N) and SC : O(N)
+class Solution {
+public:
+    vector<int> buildArray(vector<int>& nums) {
+        int n = nums.size();
+    vector<int> ans(n);
+    for (int i = 0; i < n; ++i) {
+        ans[i] = nums[nums[i]];
+    }
+    return ans;
+    }
+};
