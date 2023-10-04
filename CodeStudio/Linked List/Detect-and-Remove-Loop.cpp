0b33d58202fe99108a5a6e5c71ffe0f25ba74fc4
Anmol Pal <85279423+anmolpal2001@users.noreply.github.com>
2023-10-05 00:08:07 +0530

    Create Detect-and-Remove-Loop.cpp

---
+++ CodeStudio/Linked List/Detect-and-Remove-Loop.cpp
@@ -0,0 +1,66 @@
+Ques : https://www.codingninjas.com/studio/problems/interview-shuriken-42-detect-and-remove-loop_241049
+
+Solution : 
+Node *flyodDetectLoop(Node *head)
+{
+    if(head==NULL)
+    {
+        return NULL;
+    }
+    Node *fast=head;
+    Node *slow=head;
+    while(slow !=NULL && fast!=NULL)
+    {
+        fast=fast->next;
+        if(fast!=NULL)
+        {
+            fast=fast->next;
+        }
+        slow=slow->next;
+        if(slow==fast)
+        {
+            return slow;
+        }
+    }
+    return NULL;
+}
+
+Node *getStartingNode(Node *head)
+{
+    if(head==NULL)
+    {
+        return NULL;
+    }
+    Node * intersection = flyodDetectLoop(head);
+    if(intersection==NULL)
+    {
+        return  NULL;
+    }
+    Node *slow=head;
+    while(slow!=intersection)
+    {
+        slow=slow->next;
+        intersection=intersection->next;
+    }
+    return  slow;
+}
+
+Node *removeLoop(Node *head)
+{   
+    if(head==NULL)
+    {
+        return NULL;
+    }
+    Node * startNode = getStartingNode(head);
+    if(startNode == NULL)
+    {
+        return head;
+    }
+    Node * temp = startNode;
+    while(temp->next!=startNode)
+    {
+        temp=temp->next;
+    }
+    temp->next=NULL;
+    return head;
+}
