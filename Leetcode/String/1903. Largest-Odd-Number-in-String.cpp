c5d9086650f1c2e423818476c459647cb88724e0
Anmol Pal <85279423+anmolpal2001@users.noreply.github.com>
2023-10-16 00:31:40 +0530

    Create 1903. Largest-Odd-Number-in-String.cpp

---
+++ Leetcode/String/1903. Largest-Odd-Number-in-String.cpp
@@ -0,0 +1,16 @@
+Ques : https://leetcode.com/problems/largest-odd-number-in-string/
+
+Solution : 
+class Solution {
+public:
+    string largestOddNumber(string num) {
+        for(int i = num.size()-1; i >= 0; i--)
+        {
+            if((int)(num[i] - 48) % 2 != 0)
+            {
+                return num.substr(0, i+1);
+            }
+        }
+        return "";
+    }
+};
